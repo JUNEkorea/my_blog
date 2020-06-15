17e890d23aa72a216a88e695ad3de036d79a41dc
JUNEkorea <junnykorea2@gmail.com>
2020-06-15 18:51:38 +0900

    사이드바작업

--- part/head.php
+++ part/head.php
@@ -2,11 +2,12 @@
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
 </head>
 <body>
@@ -17,22 +18,29 @@
             <div></div>
         </a>
     </div>
+    <div class="mobile-side-bar-bg"></div> 
+    <div class="mobile-side-bar">
+        <div class="menu-box-2"><ul>
+            <li><a href="#"><i class="fas fa-globe"></i> &nbsp&nbsp myPlan</a></li>
+            <li><a href="#"><i class="fas fa-republican"></i> &nbsp&nbsp myPlace</a></li>
+            <li><a href="#"><i class="fas fa-user-friends"></i> &nbsp&nbsp myPerson</a></li>
+            <li><a href="#"><i class="fas fa-icons"></i> &nbsp&nbsp myPart</a></li>
+        </ul></div>
+    </div>
 
-    <div class="top-bar">
-        <div class="con height-100p flex">
-            <a href="/" class="logo flex flex-ai-c">
-                <i class="fas fa-clone"></i>
-            </a>
+    <div class="top-bar visible-md-up">
+        <div class="logo-box con height-100p flex">
+            <a href="/" class="logo flex flex-ai-c"><i class="fas fa-robot"></i></a>
+
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
-                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
-                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
+                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">MyPlan</a></li>
+                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPlace</a>
                     </li>
-                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPart</a>
                     </li>
-                    <li class="flex-1-0-0 flex"><a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a></li>
                     <li class="flex-1-0-0 flex">
-                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
+                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPeople</a>
                         <ul>
                             <li>
                                 <a href="https://github.com/jhs512" target="_blank">
@@ -54,6 +62,8 @@
                 </ul>
             </nav>
         </div>
-    </div> 
+    </div>
 
-   
+    <div class="page-1"><div class="img-box"><img src="https://image.freepik.com/free-photo/woman-doing-thumbs-up-gesture-e-learning-concept_23-2148550699.jpg" alt=""></div></div>
+
+    
--- public/index.php
+++ public/index.php
@@ -1,34 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>블로그</title>
-    <link rel="stylesheet" href="/resource/common.css">
-    <script src="/resource/common.js"></script>
-</head>
-<body>
-    <div class="top-bar">
-        <div class="con height-100p flex">
-            <nav class="menu-box-1 flex flex-1-0-0">
-                <ul class="flex flex-1-0-0">
-                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
-                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
-                    </li>
-                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
-                    </li>
-                    <li class="flex-1-0-0 flex"><a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
-                    <ul class ="flex">
-                        <li class ="flex "><a href="https://www.youtube.com/channel/UCBTWaHNycs3pqXgt5v3nGGQ?view_as=subscriber "class = "block">나의 유투브 </a></li>
-                        <li><a href="https://junnykorea.tistory.com/ "class = "block">나의 블로그 </a></li>
-                    </ul>   
-
-                </li>
-                </ul>
-            </nav>
-        </div>
-    </div> 
-
-    </body>
-</html>
+<?php
+include "../part/head.php";
+?>
+<h1 class="con">main</h1>
+<?php
+include "../part/foot.php";
+?>
