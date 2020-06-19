516fefa31ef826df73c228a87405ffd6f557db95
JUNEkorea <junnykorea2@gmail.com>
2020-06-19 18:07:32 +0900

    상단페이지

--- part/head.php
+++ part/head.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>이지윤 블로그</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="/resource/common.css">
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
+    <link rel="icon" href="./resource/img/wing3.png">
 </head>
+
 <body>
     <div class="mobile-top-bar visible-sm-down flex">
         <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
@@ -18,7 +24,7 @@
             <div></div>
         </a>
     </div>
-    <div class="mobile-side-bar-bg"></div> 
+    <div class="mobile-side-bar-bg"></div>
     <div class="mobile-side-bar">
         <div class="menu-box-2">
             <ul>
@@ -35,12 +41,12 @@
                     <a href="#"><i class="fas fa-user-friends"></i> &nbsp&nbsp myPerson</a>
                     <ul>
                         <li>
-                            <a href="#" >
+                            <a href="#">
                                 GITHUB
                             </a>
                         </li>
                         <li>
-                            <a href="#" >
+                            <a href="#">
                                 FACEBOOK
                             </a>
                         </li>
@@ -54,8 +60,8 @@
                                 </li>
                                 <li>
                                     <a href="#">내용</a>
-                                </li>   
-                            </ul>  
+                                </li>
+                            </ul>
 
                         </li>
                     </ul>
@@ -66,14 +72,16 @@
 
     <div class="top-bar visible-md-up">
         <div class="logo-box con height-100p flex">
-            <a href="/" class="logo flex flex-ai-c"><i class="fas fa-robot"></i></a>
+            <a href="/" class="logo flex flex-ai-c"><img src="/resource/img/leaf2.png" alt=""></a>
 
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
                     <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">MyPlan</a></li>
-                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPlace</a>
+                    <li class="flex-1-0-0 flex"><a href="/list.php"
+                            class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPlace</a>
                     </li>
-                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPart</a>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php"
+                            class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPart</a>
                     </li>
                     <li class="flex-1-0-0 flex">
                         <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">myPeople</a>
@@ -100,6 +108,32 @@
         </div>
     </div>
 
-    <div class="page-1"><div class="img-box"><img src="https://image.freepik.com/free-photo/woman-doing-thumbs-up-gesture-e-learning-concept_23-2148550699.jpg" alt=""></div></div>
+    <div class="sbs-slider">
+        <div class="slides">
+            <div style="background-image:url(https://cdn.pixabay.com/photo/2020/06/15/15/43/couple-5302344_960_720.jpg);"
+                class="active"></div>
+            <div
+                style="background-image:url(https://cdn.pixabay.com/photo/2014/02/07/11/36/couple-260899_960_720.jpg);">
+            </div>
+            <div
+                style="background-size:contain; background-image:url(https://cdn.pixabay.com/photo/2016/10/06/05/19/engagement-1718244_960_720.jpg);">
+            </div>
+            <div
+                style="background-size:contain; background-image:url(https://cdn.pixabay.com/photo/2015/11/07/11/01/couple-1030744_960_720.jpg);">
+            </div>
+            <div
+                style="background-size:contain; background-image:url(https://cdn.pixabay.com/photo/2015/03/30/20/33/heart-700141_960_720.jpg);">
+            </div>
+        </div>
 
-    
+        <div class="side-bars">
+            <div>
+                <span>
+                    <i class="fa fa-angle-left" aria-hidden="true"></i>
+                </span>
+            </div>
+            <div>
+                <span><i class="fa fa-angle-right" aria-hidden="true"></i></span>
+            </div>
+        </div>
+    </div>
--- public/resource/common.js
+++ public/resource/common.js
@@ -4,40 +4,78 @@ function MobileSideBar__toggle() {
   var $mobileSideBarBg = $('.mobile-side-bar-bg');
   var $html = $('html');
 
-  if ( $btn.hasClass('active') ) {
-      // 모바일 사이드바 끄기
-      $btn.removeClass('active');
-      $mobileSideBar.removeClass('active');
-      $mobileSideBarBg.removeClass('active');
-      $html.removeClass('mobile-side-bar-actived');
-  }
-  else {
-      // 모바일 사이드바 켜기
-      $btn.addClass('active');
-      $mobileSideBar.addClass('active');
-      $mobileSideBarBg.addClass('active');
-      $html.addClass('mobile-side-bar-actived');
+  if ($btn.hasClass('active')) {
+    // 모바일 사이드바 끄기
+    $btn.removeClass('active');
+    $mobileSideBar.removeClass('active');
+    $mobileSideBarBg.removeClass('active');
+    $html.removeClass('mobile-side-bar-actived');
+  } else {
+    // 모바일 사이드바 켜기
+    $btn.addClass('active');
+    $mobileSideBar.addClass('active');
+    $mobileSideBarBg.addClass('active');
+    $html.addClass('mobile-side-bar-actived');
   }
 }
 
 function MobileSideBar__init() {
   $('.btn-toggle-mobile-side-bar, .mobile-side-bar-bg').click(MobileSideBar__toggle);
 
-  $('.mobile-side-bar .menu-box-2 ul > li').click(function(e) {
-      if ( $(this).hasClass('active') ) {
-          $(this).removeClass('active');
-      }
-      else {
-          $(this).addClass('active');
-      }
+  $('.mobile-side-bar .menu-box-2 ul > li').click(function (e) {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).addClass('active');
+    }
 
-      e.stopPropagation();
+    e.stopPropagation();
   });
 }
 
-$(function() {
-  MobileSideBar__init();
+function slider() {
+  var $clickedBtn = $(this);
+    // closest => 조상중에서 가장 가까운 엘리먼트 하나 찾아주세요.
+    var $slider = $clickedBtn.closest('.sbs-slider');
 
-  $('.btn-toggle-mobile-side-bar').click();
-  $('.mobile-side-bar .menu-box-2 > ul > li:last-child').click();
+    //.index() => 형제번호
+    var isLeft = $clickedBtn.index() == 0;
+
+    // find => 지역탐색
+    // $currnet => 현재 active 된 녀석
+    var $currnet = $slider.find('.slides > .active');
+    // next => 다음 형제 가져와
+    var $post = null;
+
+    if (isLeft) {
+      $post = $currnet.prev();
+    } else {
+      $post = $currnet.next();
+    }
+
+    if ($post.length == 0) {
+      if (isLeft) {
+        $post = $slider.find('.slides > div:last-child');
+      } else {
+        $post = $slider.find('.slides > div:first-child');
+      }
+    }
+
+    $currnet.removeClass('active');
+    $post.addClass('active');
+}
+
+
+
+function Slider__init() {
+  $('.sbs-slider > .side-bars > div').click(slider);
+  setInterval(function(){
+    $('.sbs-slider > .side-bars > div:last-child').click()
+  }, 5000)
+}
+
+
+$(function () {
+  MobileSideBar__init();
+  Slider__init();
 });
