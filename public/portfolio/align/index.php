f59670b6789dd1bd60279a381aa23ebc66329161
JUNEkorea <junnykorea2@gmail.com>
2020-07-09 18:40:34 +0900

    align main page

---
+++ public/portfolio/align/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="align/index.css">
+</head>
+
+<body>
+    <div class="top-bar-box">
+        <div class="top-bar flex con">
+            <div class="logo flex-ai-c"><a href="#"><img src="http://www.kocom-align.com/thema/Align/logo-top.png"
+                        alt=""></a>
+            </div>
+            <nav class="menu-box-1 flex ">
+                <ul class="flex flex-ai-c">
+                    <li><a href="#">T9</a></li>
+                    <li><a href="#">M9</a></li>
+                    <li><a href="#">T9lot</a></li>
+                    <li><a href="#">UI</a></li>
+                </ul>
+            </nav>
+        </div>
+
+
+    </div>
+
+    <div class="page-1">
+        <div class="copy-1">코콤이 만든 <span style="font-weight:600">프리미엄</span> 브랜드입니다.</div>
+    </div>
+
+    <div class="side-right-menu flex">
+        <ul>
+                <li><a href="#">Align</a></li>
+                <li><a href="#">Move</a></li>
+                <li><a href="#">First Premium</a></li>
+                <li><a href="#">Vision & Mission</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">Partner</a></li>
+                <li><a href="#">Performance</a></li>
+                <li><a href="#">Align</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
