9ef98ed401ca4e76abc21ad77f36a11259a5321f
JUNEkorea <junnykorea2@gmail.com>
2020-07-19 21:00:55 +0900

    rollerpaster2page시작

--- public/portfolio/rollingpasta/index.php
+++ public/portfolio/rollingpasta/index.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>nonghyup</title>
+    <title>RollingPasta</title>
 
     <!-- 제이쿼리 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- 폰트 어썸 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 
     <!-- owl 캐러셀 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
@@ -20,9 +23,6 @@
 
     <!-- 스크롤리 파이 불러오기 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.19/jquery.scrollify.min.js"></script>
-
-    <!-- 폰트 어썸 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 
     <!-- 페이지용 -->
     <link rel="stylesheet" href="/portfolio/rollingpasta/index.css">
@@ -40,12 +40,12 @@
         </a>
         <!-- 가운데 메뉴바   -->
         <nav class="menu-box-1 text-align-center line-height-0-ch-only">
-            <ul class="row inline-block">
-                <li class="cell page-1-menu-item"><a href="#page-1" class="block">Rolling Pasta</a></li>
-                <li class="cell page-2-menu-item"><a href="#page-2" class="block">Menu</a></li>
-                <li class="cell page-3-menu-item"><a href="#page-3" class="block">Store</a></li>
-                <li class="cell page-4-menu-item"><a href="#page-4" class="block">Franchise</a></li>
-                <li class="cell page-5-menu-item cs-center-cell">
+            <ul class="row inline-block"><!-- 페이지2부터연결시작. -->
+                <li class="cell page-2-menu-item"><a href="#page-2" class="block">Rolling Pasta</a></li>
+                <li class="cell page-3-menu-item"><a href="#page-3" class="block">Menu</a></li>
+                <li class="cell page-4-menu-item"><a href="#page-4" class="block">Store</a></li>
+                <li class="cell page-5-menu-item"><a href="#page-5" class="block">Franchise</a></li>
+                <li class="cell page-6-menu-item cs-center-cell">
                     <a href="#page-6" class="block relative">
                         CS center
                         <span class="ico ico-1 absolute-right absolude-middle"></span>
@@ -54,13 +54,13 @@
             </ul>
         </nav>
     </div>
-    <!-- 스크롤리파이적용 -->
+    <!-- 중간 오른쪽 끝- 스크롤리파이적용 -->
     <div class="page-indicator-box"></div>
 
     <!-- 팝업 박스 만들기 -->
     <div class="popup-box-1">
         <div class="img-box">
-            <img src="http://junekorea.github.io/img1/ph/rollingpasta/p1.jpg" alt="">
+            <!-- <img src="http://junekorea.github.io/img1/ph/rollingpasta/p1.jpg" alt=""> -->
             <img src="http://junekorea.github.io/img1/ph/rollingpasta/p2.jpg" alt="">
         </div>
         <div class="row">
@@ -107,6 +107,9 @@
         </div>
         <div data-section-name="page-5" class="page-5 page">
             <h1>페이지5</h1>
+        </div>
+        <div data-section-name="page-6" class="page-5 page">
+            <h1>페이지6</h1>
         </div>
     </div>
 
